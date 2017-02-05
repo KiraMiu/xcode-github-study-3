18a0f727cd0025e6ca4e54b9c62b2d2b4b3029fc
Botin Bai <baozi88748834@qq.com>
2017-02-05 16:17:03 +0800

    版本描述：在已有的远程仓库下，新建一个oc项目，并添加了一行测试代码。第一次提交版本。

---
+++ github学习_2_0205/github学习_2_0205/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github学习_2_0205
+//
+//  Created by BotinBai on 2017/2/5.
+//  Copyright © 2017年 MIU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"第一次commit");
+    }
+    return 0;
+}
